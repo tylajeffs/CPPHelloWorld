92652b11fca9541788c527f05d29e82eddb5224a
Tyla Jeffs <40805521+tylajeffs@users.noreply.github.com>
2022-01-07 11:42:04 -1000

    Add files via upload

---
+++ CPPHelloWorld.cpp
@@ -0,0 +1,35 @@
+
+
+//import but not
+#include <iostream>
+
+//
+using namespace std;
+
+int main()
+{
+    //print stuff
+    std::cout << "\"I'm Glad the Sky is Painted Blue\n";
+    std::cout << "\"I'm glad the sky is painted blue,\n";
+    std::cout << "\"And the earth is painted green,\n";
+    std::cout << "\"With such a lot of nice fresh air\n";
+    std::cout << "\"All sandwiched in between.\n";
+
+
+    //always returns 0 if no errors
+    return 0;
+}
+
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
